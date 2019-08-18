bug correction + self-reference masking for sequential input + minor refactorings

onmt/encoders/selectrans.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 
 from onmt.inputters.inputter import load_old_vocab, old_style_vocab
 from onmt.encoders.transformer import TransformerEncoder
+from onmt.utils.misc import get_definienda_from_src
 
 SLCT_label = "SLCT"
 
@@ -65,8 +66,4 @@ class SelecTransEncoder(TransformerEncoder):
 
     def clean_src(self, src):
         # TODO: remove that fix...
-        true_input, select_labels = torch.unbind(src, dim=-1)
-        selector_mask = (select_labels == self._select_idx)
-        select_src = torch.masked_select(true_input, selector_mask)
-        # one element, batched, one feature -> [1 x B x 1]
-        return select_src.view(1, src.size(1), 1)
+        return get_definienda_from_src(src, self._select_idx)

onmt/utils/loss.py
@@ -205,22 +205,25 @@ class LabelSmoothingLoss(nn.Module):
         """
         output (FloatTensor): batch_size x n_classes
         target (LongTensor): batch_size
-        sr_mask (ByteTensor): src_ipt_size x src_batch_size x n_classes
+        sr_mask (ByteTensor): 1 x src_batch_size x n_classes
         """
         if sr_mask is not None:
-            mb = output.view(-1, sr_mask.size(1), output.size(1)).size(0)
+            mb = output.size(0) // sr_mask.size(1)
             smooth = (
                 self.label_smoothing /
                 (output.size(-1) - (2+sr_mask.squeeze(0).sum(dim=-1).float()))
             ).unsqueeze(1)
             model_prob = (~sr_mask).squeeze(0).float() * smooth
-            model_prob[:self.ignore_index] = 0
+            model_prob[:,self.ignore_index] = 0
             model_prob = model_prob.expand(mb, *model_prob.size()).contiguous().view(-1, output.size(1))
         else:
             model_prob = self.one_hot.repeat(target.size(0), 1)
         model_prob.scatter_(1, target.unsqueeze(1), self.confidence)
         model_prob.masked_fill_((target == self.ignore_index).unsqueeze(1), 0)
-
+        if sr_mask is not None:
+            mb = output.size(0) // sr_mask.size(1)
+            sr_mask = sr_mask.expand(mb, *sr_mask.size()).contiguous().view(-1, output.size(1))
+            model_prob.masked_fill_(sr_mask, 0)
         return F.kl_div(output, model_prob, reduction='sum')
 
 